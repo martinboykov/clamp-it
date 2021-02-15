c13ece1a74a2a2b079c90db93193c5518be2beeb
Martin Martinov <m.martinov@kipo.bg>
2021-02-15 18:19:08 +0200

    docs

---
+++ docs/precache-manifest.c470f7ffb946cf68a7723718de667cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ea898bcf9e909e68450ca446d9c99f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ced644a2e14c8a0889a5",
+    "url": "/static/css/main.ccdbfb76.chunk.css"
+  },
+  {
+    "revision": "cbfb379bd174650acb57",
+    "url": "/static/js/2.7b9be4bd.chunk.js"
+  },
+  {
+    "revision": "ced644a2e14c8a0889a5",
+    "url": "/static/js/main.597aa497.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87296f22099264737ae19173ae9a7a08",
+    "url": "/static/media/copy.87296f22.svg"
+  }
+]);
